f69f9950ade6475cb924a7cace2a6fd46d3c7b3d
Thiago Matias Martins <41652032+mmt93@users.noreply.github.com>
2023-06-30 09:53:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+VERSION = '0.0.1' 
+DESCRIPTION = 'Hallucination filter for LLM'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
 setup(
-    name='llmhalufilter',
-    version='0.1',
-    scripts=['main.py'],
+       # the name must match the folder name 'verysimplemodule'
+        name="hallu_filter", 
+        version=0.0.1,
+        description=preventing hallucinations,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
 )
